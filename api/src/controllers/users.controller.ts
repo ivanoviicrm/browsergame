c73f1de2c91fa2a13f8c4fe0cb69a312762a5788
ivanrodriguez <ivanoviicrm2@gmail.com>
2021-08-08 15:59:37 +0200

    working on db connection

--- api/src/controllers/users.controller.ts
+++ api/src/controllers/users.controller.ts
@@ -1,7 +1,8 @@
 import { Application, Request, Response } from 'express';
 import { UsersService } from '../services/users.service';
+import { pool } from '../database/common/constants';
 
-const usersService = new UsersService();
+const usersService = new UsersService(pool);
 
 export const usersController = (app: Application): void => {
   app.get('/users', async (req: Request, res: Response) => {
--- api/src/services/users.service.ts
+++ api/src/services/users.service.ts
@@ -1,28 +1,28 @@
 import { User } from '../database/models/users.model';
-import { CRUD, pool } from '../database/common/constants';
+import { CRUD } from '../database/common/constants';
 import { Pool } from 'mariadb';
 
 export class UsersService implements CRUD {
-  pool: Pool;
+  db: Pool;
   tableName: string;
 
-  constructor() {
-    this.pool = pool;
+  constructor(pool: Pool) {
+    this.db = pool;
     this.tableName = 'users';
   }
 
   findAll(limit?: number | undefined, page?: number | undefined): Promise<any> {
-    return pool.query(`SELECT * FROM ${this.tableName};`);
+    return this.db.query(`SELECT * FROM ${this.tableName};`);
   }
 
   findById(id: number): Promise<any> {
-    return pool.query(`SELECT * FROM ${this.tableName} WHERE 'id' = ${id};`);
+    return this.db.query(`SELECT * FROM ${this.tableName} WHERE 'id' = ${id};`);
   }
 
   create(user: User): Promise<any> {
     const values = Object.values(user).toLocaleString();
 
-    return pool.query(`
+    return this.db.query(`
     INSERT INTO ${this.tableName}(id, name, email, password, createdAt, updatedAt, active)
     VALUES(${values});`);
   }
@@ -30,13 +30,13 @@ export class UsersService implements CRUD {
   update(id: number, user: User): Promise<any> {
     const values = Object.values(user).toLocaleString();
 
-    return pool.query(`
+    return this.db.query(`
       INSERT INTO ${this.tableName}(id, name, email, password, createdAt, updatedAt, active)
       VALUES(${values})
       WHERE 'id' = ${id};`);
   }
 
   deleteById(id: number): Promise<any> {
-    return pool.query(`DELETE FROM ${this.tableName} WHERE 'id' = ${id};`);
+    return this.db.query(`DELETE FROM ${this.tableName} WHERE 'id' = ${id};`);
   }
 }
